ee66283c08caae029ded1dbbd965e43fb843b8b7
nhatkhanh224 <55695197+nhatkhanhl@users.github.com>
2020-11-01 11:03:54 +0700

    View Controller

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const exphbs = require("express-handlebars");
 const app = express();
 const http = require("http");
+const route=require("./routes");
 app.set("port", process.env.PORT || 3000);
 app.use(express.static(path.join(__dirname, "public")));
 //Http logger
@@ -17,14 +18,8 @@ app.engine(
 );
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
-
-app.get("/", function (req, res) {
-  res.render("home");
-});
-app.get("/news", function (req, res) {
-  res.render("news");
-});
-
+//Routes init
+route(app);
 http.createServer(app).listen(app.get("port"), function () {
   console.log("Express server listening on port " + app.get("port"));
 });
